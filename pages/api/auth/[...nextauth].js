e019ed59b366699d7c734916045e92369381b416
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-16 16:35:13 +0300

    tap the user access token and refresh token

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -66,5 +66,12 @@ export default NextAuth({
     return await refreshAccessToken(token)
 
     },
+    async session({session, token}) {
+        session.user.accessToken = token.accessToken;
+        session.user.refreshToken = token.refreshToken;
+        session.user.username = token.username;
+
+        return session;
+    }
   }
-})
+});
