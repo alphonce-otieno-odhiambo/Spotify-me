6aaa457e0b3bb794b2d90988553b9ccc0e19fed9
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-16 16:48:57 +0300

    pass in the login provider

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import {getProviders, signIn} from 'next-auth/react';
 
-function login() {
+function login({providers}) {
   return (
-    <div>login</div>
+    <div>
+        <img src='https://links.papareact.com/9xl' alt='' 
+        className=' w-30 mb-5'/>
+        {Object.values(providers)}
+    </div>
   )
 }
 
-export default login
+export default login;
+
+export async function getServerSideProps(){
+    const providers = await getProviders();
+    return {
+        props:{
+            providers,
+        }
+    }
+}
