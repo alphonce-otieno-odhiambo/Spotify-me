1c69a7ff6bceae55d82f688cba24e430dcd3d767
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-16 16:27:20 +0300

    set wjt function

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,35 @@
 import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/spotify"
-import { LOGIN_URL } from "../../../lib/spotify"
+
+import spotifyApi, { LOGIN_URL } from "../../../lib/spotify"
+
+
+async function refreshAccessToken(token){
+    try {
+
+        spotifyApi.setAccessToken(token.accessToken);
+        spotifyApi.setRefreshToken(token.refreshToken);
+
+        const {body:refreshedToken } = await spotifyApi.refreshAccessToken();
+        console.log("REFRESHED TOKEN IS", refreshedToken);
+
+        return {
+            ...token,
+            accessToken:refreshedToken.access_token,
+            accessTokenExpires: Date.now + refreshedToken.expires_in * 1000 ,// = 1hr as 3600 returns from spotify api
+            refreshedToken:refreshedToken.refresh_token ?? token.refreshToken,
+        }
+
+        
+    } catch (error) {
+        console.log(error)
+
+        return {
+            ...token,
+            error:'refreshTokenError'
+        }
+    }
+}
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -13,4 +42,29 @@ export default NextAuth({
     // ...add more providers here
   ],
   secret: process.env.JWT_SECRET,
+  pages:{
+      signIn:'/login'
+  },
+  callbacks: {
+    async jwt({token, account , user}){
+        // innitial signIn 
+      if(account && user ){
+          return {
+            ...token,
+            accessToken:account.access_token,
+            refreshToken:account.refresh_token,
+            username:account.providerAccountId,
+            accessTokenExpires:account.expires_at * 1000,
+          }
+      }
+    // Return the prevouse token if access token has not expired
+    if(Date.now() < token.accessTokenExpires){
+        return token;
+    }
+
+    //Access token expires, we need to refresh it..
+    return await refreshAccessToken(token)
+
+    },
+  }
 })
